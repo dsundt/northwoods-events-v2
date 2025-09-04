dfe8071af33fef8844a4475fd82f68c6138cf4b0
dsundt <dsundt@gmail.com>
2025-09-03 22:27:48 -0500

    Update init.py

--- src/parsers/init.py
+++ src/parsers/init.py
@@ -1,11 +1,10 @@
+# Expose fetchers for main.py
 from .tec_rest import fetch_tec_rest
-from .tec_auto import fetch_tec_auto
 from .growthzone_html import fetch_growthzone_html
 from .simpleview_html import fetch_simpleview_html
 
 __all__ = [
     "fetch_tec_rest",
-    "fetch_tec_auto",
     "fetch_growthzone_html",
     "fetch_simpleview_html",
 ]
