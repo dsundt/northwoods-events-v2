f16b0a5a63174db3d3ce81c5b52b4f7c2de724db
dsundt <dsundt@gmail.com>
2025-09-03 20:49:09 -0500

    Update __init__.py

--- src/parsers/__init__.py
+++ src/parsers/__init__.py
@@ -2,3 +2,4 @@ from .ics_parser import parse_ics_feed
 from .tec_parser import parse_tec_html
 from .growthzone_parser import parse_growthzone_html
 from .ai1ec_parser import parse_ai1ec_html
+from .simpleview_parser import parse_simpleview_html  # NEW
