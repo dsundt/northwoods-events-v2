2208b66239f8d83d961fe9dfe91c7876aeea8aae
dsundt <dsundt@gmail.com>
2025-09-04 17:37:00 -0500

    Update init.py

--- src/parsers/init.py
+++ src/parsers/init.py
@@ -1,10 +1,14 @@
-# Expose fetchers for main.py
-from .tec_rest import fetch_tec_rest
+# src/parsers/__init__.py
+
+from .tec_rest import fetch_tec_rest, TEC_DEFAULT_WINDOW_DAYS
+from .tec_html import fetch_tec_html
 from .growthzone_html import fetch_growthzone_html
 from .simpleview_html import fetch_simpleview_html
 
 __all__ = [
     "fetch_tec_rest",
+    "TEC_DEFAULT_WINDOW_DAYS",
+    "fetch_tec_html",
     "fetch_growthzone_html",
     "fetch_simpleview_html",
 ]
